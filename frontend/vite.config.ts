8c4e1be4cbbb712c796e72acb5f51776755c56fb
AndreaBarrera28 <112991492+AndreaBarrera28@users.noreply.github.com>
2025-05-12 23:12:29 -0600

    feat: Add certificates gh-5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig, loadEnv } from "vite";
+import plugin from "@vitejs/plugin-react";
+import fs from "fs";
+import * as https from "https";
+
+export default ({ mode }: { mode: string }) => {
+    Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+    const options: https.ServerOptions = {
+        cert: process.env.SSL_CRT_FILE
+            ? fs.readFileSync(process.env.SSL_CRT_FILE)
+            : "",
+        key: process.env.SSL_KEY_FILE
+            ? fs.readFileSync(process.env.SSL_KEY_FILE)
+            : "",
+    };
+    // https://vitejs.dev/config/
+    return defineConfig({
+        plugins: [plugin()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: "http://localhost:8000", //debe dee estar igual al .env en cuanto a http o https
+                    secure: false,
+                    changeOrigin: true,
+                    //rewrite: (path) => path.replace(/^\/api/, ""), //esto no ... da error
+                },
+            },
+            port: 5173,
+            https: options,
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: "static/js/[name].js", // Custom name for entry files
+                    chunkFileNames: "static/js/[name].js", // Custom name for chunks
+                    assetFileNames: "static/assets/[name].[ext]", // Custom name for assets
+                },
+            },
+        },
+        css: {
+            devSourcemap: false,
+          },
+    });
+};
