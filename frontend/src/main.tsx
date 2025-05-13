5bf56658ff0fb31ebc448554481b36989832be34
AndreaBarrera28 <112991492+AndreaBarrera28@users.noreply.github.com>
2025-05-12 23:29:16 -0600

    New fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,26 +1,12 @@
-import React from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { CookiesProvider } from "react-cookie";
-import { CustomProvider } from "rsuite";
 import router from "./routes";
-
-const queryClient = new QueryClient();
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
-   <React.StrictMode>
-        <CustomProvider theme="dark">
-            <CookiesProvider>
-                <QueryClientProvider client={queryClient}>
-                    {/* <UserContextProvider> */}
-                        <RouterProvider router={router} />
-                    {/* </UserContextProvider> */}
-                    <ReactQueryDevtools />
-                </QueryClientProvider>
-            </CookiesProvider>
-        </CustomProvider>
-    </React.StrictMode>
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
